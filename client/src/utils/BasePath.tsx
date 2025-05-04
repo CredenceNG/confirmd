0623c7a5c4b234028f6e89b53b3283396b952a41
itopsa <itopa@adaptiflo.com>
2025-05-04 02:12:37 -0400

    Basepath Edits

--- client/src/utils/BasePath.tsx
+++ client/src/utils/BasePath.tsx
@@ -1,3 +1,2 @@
-import { baseRoute } from '../api/BaseUrl'
-
-export const basePath: string = baseRoute ?? ''
+// This should be in your utils/BasePath.ts or similar file
+export const basePath = process.env.REACT_APP_BASE_ROUTE || '';
